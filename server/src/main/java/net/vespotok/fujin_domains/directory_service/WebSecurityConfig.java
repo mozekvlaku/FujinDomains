0d22f93b842bb726cdc2ca123ff9314868626b17
mozekvlaku <t.kracik@seznam.cz>
2022-02-02 17:22:44 +0100

    CORS oprava

--- server/src/main/java/net/vespotok/fujin_domains/directory_service/WebSecurityConfig.java
+++ server/src/main/java/net/vespotok/fujin_domains/directory_service/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.csrf().disable().cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 
     @Bean
